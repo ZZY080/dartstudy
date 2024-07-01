dc194fad48c89da17377e61a90f8540d2068d689
kenny <2916363651@qq.com>
2024-07-01 15:52:54 +0800

    dart修改

--- demo14/05 Dart 导入Pub包管理系统中的库.dart
+++ demo14/05 Dart 导入Pub包管理系统中的库.dart
@@ -23,20 +23,17 @@ pub包管理系统:
 */
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
-// import 'package:date_format/date_format.dart';
+import 'package:date_format/date_format.dart';
 
 main() async {
-  // var url = "http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=1";
-
+  var uri = new Uri.http('news-at.zhihu.com', '/api/3/stories/latest');
   //   // Await the http get response, then decode the json-formatted responce.
-  //   var response = await http.get(url);
-  //   if (response.statusCode == 200) {
-  //     var jsonResponse = convert.jsonDecode(response.body);
-
-  //     print(jsonResponse);
-  //   } else {
-  //     print("Request failed with status: ${response.statusCode}.");
-  //   }
-
-  // print(formatDate(DateTime(1989, 2, 21), [yyyy, '*', mm, '*', dd]));
+  var response = await http.get(uri);
+  if (response.statusCode == 200) {
+    var jsonResponse = convert.jsonDecode(response.body);
+    print(jsonResponse);
+  } else {
+    print("Request failed with status: ${response.statusCode}.");
+  }
+  print(formatDate(DateTime(1989, 2, 21), [yyyy, '*', mm, '*', dd]));
 }
